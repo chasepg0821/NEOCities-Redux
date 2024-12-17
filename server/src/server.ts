c5aea078dbb7258739c494134f1fedee8f4c560a
Chase Guynup <chasepg0821@gmail.com>
2024-12-16 20:44:51 -0500

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express, { type Express } from "express";
+import { pino } from "pino";
+import { env } from "@/common/utils/envConfig";
+
+const logger = pino({ name: "server start" });
+const app: Express = express();
+
+// Set the application to trust the reverse proxy
+app.set("trust proxy", true);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+
+export { app, logger };
